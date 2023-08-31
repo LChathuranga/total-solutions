761f027739b32a07a92f285d86220b3cf2656560
@lahiruchathuranga19981214 <lahiruchathuranga19981214@gmail.com>
2023-09-01 00:55:55 +0530

    CHG | Bottom of home page

--- wp-content/plugins/wp-whatsapp-chat/build/backend/js/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('backbone', 'jquery', 'jquery-ui-sortable', 'qlwapp-select2', 'wp-color-picker', 'wp-util'), 'version' => 'af7b289ab1b3d4ca271e');
--- wp-content/plugins/wp-whatsapp-chat/build/frontend/js/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '9ae13648aa1aa7954736');
